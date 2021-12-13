cc28312a4ed39ed2e7a333819ba81bd6a62f99a4
Alan Shi <v-alanshi@microsoft.com>
2021-12-13 15:40:49 +0800

    22

--- EMailHelper.cs
+++ EMailHelper.cs
@@ -12,47 +12,6 @@ namespace AutomationHelper
 {
     public class EMailHelper
     {
-        public static void SendMailServer(string title, string target, string text, bool isHtml)
-        {
-            SmtpClient mail = new SmtpClient(getXML("Host"),Convert.ToInt32(getXML("port")));
-            string userName = getXML("userName");
-            mail.UseDefaultCredentials = false;
-            mail.Credentials = new NetworkCredential(userName + "@microsoft.com", getXML("FromPwd"));
-            mail.EnableSsl = true;
-            string subject = string.Format("{0}:{1}", title, CommonHelper.GetNowString());
-            MailMessage msg = new MailMessage();
-            //msg.From = new MailAddress(System.Environment.UserName + "@microsoft.com");
-            msg.From = new MailAddress(userName + "@microsoft.com");
-            msg.Subject = subject;
-            List<string> tos = ParseToTarget(target);
-            foreach (string to in tos)
-            {
-                msg.To.Add(to);
-            }
-            msg.IsBodyHtml = true;
-            msg.Body = text;
-            try
-            {
-                mail.Send(msg);
-            }
-            catch (Exception ex)
-            {
-                //LogHelper.DumpLog("SendMail Something is wrong", ex.ToString(),"log",DateTime.Now);
-                //SendMailToDev(ex.ToString());
-            }
-        }
-
-        public static string getXML(string nodeName)
-        {
-            string strReturn = "";
-            XmlDocument xdoc = new XmlDocument();
-            xdoc.Load("./Config.xml");
-            //根据路径获取节点
-            XmlNode xmlNode = null;
-            xmlNode = xdoc.SelectSingleNode("configurationN/" + nodeName);
-            strReturn = xmlNode.InnerText;
-            return strReturn;
-        }
 
 
         public static void SendMail(string title, string target, string text, bool isHtml)
